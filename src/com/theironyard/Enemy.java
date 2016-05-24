aa84dda5a51fa4ae4863eafb3a0539885c966378
Ben <bhturne@g.clemson.edu>
2016-05-24 11:10:37 -0400

    Battle

---
+++ src/com/theironyard/Enemy.java
@@ -0,0 +1,13 @@
+package com.theironyard;
+
+/**
+ * Created by Ben on 5/24/16.
+ */
+public class Enemy extends Character{
+    public Enemy(String name, int health, int damage) {
+
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+    }
+}
